0c7328f5ea40415e3dceb040d0c79bcc975cf731
santisvs <santiago.vallejo.suarez@gmail.com>
2020-11-24 18:02:09 +0100

    Update path query findByUsername

--- springboot-servicio-usuarios/src/main/java/com/practi/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/practi/app/usuarios/models/dao/UsuarioDao.java
@@ -1,13 +1,16 @@
 package com.practi.app.usuarios.models.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.practi.app.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 	
-	public Usuario findByUsername(String username);
+	@RestResource(path="buscar")
+	public Usuario findByUsername(@Param("username") String username);
 
 }
--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,9 +1,10 @@
-INSERT INTO `usuario` (username, password, enabled, name, lastname, email) VALUES ('andres','12345',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO `usuario` (username, password, enabled, name, lastname, email) VALUES ('admin','abcde',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO `rol` (name) VALUES ('ROLE_USER');
+INSERT INTO `rol` (name) VALUES ('ROLE_ADMIN');
 
---INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
---INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO `usuario` (username, password, enabled, name, lastname, email, rol_id) VALUES ('andres','12345',1, 'Andres', 'Guzman','profesor@bolsadeideas.com', 1);
+INSERT INTO `usuario` (username, password, enabled, name, lastname, email, rol_id) VALUES ('admin','abcde',1, 'John', 'Doe','jhon.doe@bolsadeideas.com', 2);
+INSERT INTO `usuario` (username, password, enabled, name, lastname, email, rol_id) VALUES ('carla','2345',1, 'Carla', 'Bene','carla07@bolsadeideas.com', 1);
 
---INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
---INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
---INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+--INSERT INTO `usuario_rol` (usuario_id, rol_id) VALUES (1, 1);
+--INSERT INTO `usuario_rol` (usuario_id, rol_id) VALUES (2, 2);
+--INSERT INTO `usuario_rol` (usuario_id, rol_id) VALUES (3, 1);
