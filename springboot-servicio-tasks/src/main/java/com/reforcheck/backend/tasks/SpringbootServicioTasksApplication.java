9e31d48008f5b6fa63e65ee2159c3658569c7eb2
santisvs <santiago.vallejo.suarez@gmail.com>
2020-12-11 16:15:30 +0100

    Init microservicio tasks

---
+++ springboot-servicio-tasks/src/main/java/com/reforcheck/backend/tasks/SpringbootServicioTasksApplication.java
@@ -0,0 +1,13 @@
+package com.reforcheck.backend.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioTasksApplication.class, args);
+	}
+
+}
---
+++ springboot-servicio-tasks/src/test/java/com/reforcheck/backend/tasks/SpringbootServicioTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reforcheck.backend.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
